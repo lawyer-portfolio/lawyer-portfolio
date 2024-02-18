79a9038f137add380fd066de783bea55993e2928
mFaYizp <fayizfaz78@gmail.com>
2024-02-19 00:32:25 +0530

    mailjs implemented

---
+++ js/mail.js
@@ -0,0 +1,27 @@
+function emailSent() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    date: document.getElementById("date").value,
+    categories: document.getElementById("categories").value,
+  };
+
+  console.log(userName, "here");
+
+  const serviceID = "service_sp98txs";
+  const templateID = "template_zuc7zpc";
+
+  emailJs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("date").value = "";
+      document.getElementById("categories").value = "";
+      console.log(res);
+      alert("Your message sent Successfully");
+    })
+    .catch((err) => console.log(err));
+}
